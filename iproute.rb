1d54b4d7e572d1708d91e7813afb10779b006a84
Marcello Barnaba <vjt@openssl.it>
2007-05-18 02:16:57 +0000

    revisited a bit, added basic link handling, removed those ugly module_function calls, added a monitoring routine with a popen over ip monitor link. this module still sucks, it should evolve into something more comprehensive above iproute, and not remain a stupid parametrized command string collection.
    git-svn-id: https://code.softmedia.info/svn/opensource/ispfailover/trunk@113 939359bc-5202-0410-adc4-d1279421971d

--- iproute.rb
+++ iproute.rb
@@ -1,60 +1,105 @@
+require 'rubygems'
+require 'facets/core/array/to_h'
+require 'thread'
+
 module IPRoute
-  def default_route
-    `ip -4 route ls 0.0.0.0/0`.strip!
-  end
-  module_function :default_route
+  Mutex = ::Mutex.new
 
-  def local_host(interface)
-    `ip -4 -o addr ls dev #{interface} scope global`.scan(/inet ((\d+\.){3}\d+)/).flatten.first
-  end
-  module_function :local_host
+  class << self
+    def default_route
+      `ip -4 route ls 0.0.0.0/0`.strip
+    end
 
-  def add_route(dest, gw, iface)
-    run "ip -4 route add #{dest} via #{gw} dev #{iface}"
-  end
-  module_function :add_route
+    def local_host(interface)
+      `ip -4 -o addr ls dev #{interface} scope global`.scan(/inet ((\d+\.){3}\d+)/).flatten.first
+    end
 
-  def del_route(dest, gw, iface)
-    run "ip -4 route del #{dest} via #{gw} dev #{iface}"
-  end
-  module_function :del_route
+    # There's a lot to beautify, here ;)
+    #
+    def with_temp_route(dest, gw, iface)
+      Mutex.synchronize do
+        begin
+          add_route dest, gw, iface, 'monitor'
+          run "ip -4 rule add to #{dest} lookup monitor"
+          yield
 
-  def add_nexthop(gw, iface, wgt = 1)
-    if default_route.empty?
-      run "ip -4 route add default #{nexthop(gw, iface, wgt)}"
-    else
-      hops = nexthops(iface).push(nexthop(gw, iface, wgt))
-      run "ip -4 route change default #{hops.join ' '}"
+        ensure
+          run "ip -4 rule del to #{dest} lookup monitor"
+          del_route dest, gw, iface, 'monitor'
+        end
+      end
+    end
+
+    def add_route(dest, gw, iface, table = 'main')
+      run "ip -4 route add #{dest} via #{gw} dev #{iface} table #{table}"
+    end
+
+    def del_route(dest, gw, iface, table = 'main')
+      run "ip -4 route del #{dest} via #{gw} dev #{iface} table #{table}"
+    end
+
+    def add_nexthop(gw, iface, wgt = 1)
+      Mutex.synchronize do
+        if default_route.nil? || default_route.empty?
+          run "ip -4 route add default #{nexthop(gw, iface, wgt)}"
+        else
+          hops = nexthops(iface).push(nexthop(gw, iface, wgt))
+          run "ip -4 route change default #{hops.join ' '}"
+        end
+      end
+    end
+
+    def del_nexthop(gw, iface)
+      Mutex.synchronize do
+        unless (hops = nexthops(iface)).empty?
+          run "ip -4 route change default #{hops.join ' '}"
+          run "ip -4 route flush cache dev #{iface}"
+        else
+          run "ip -4 route del default"
+          run "ip -4 route flush cache"
+        end
+      end
+    end
+
+    def nexthops(skip = nil)
+      hops =
+        if default_route.include? "\n"
+          default_route.split("\n").reject! { |x| x.strip! == 'default' }
+        else
+          [default_route.sub(/^default/, 'nexthop')]
+        end
+
+      hops.reject! { |x| x =~ /#{skip}/ } if skip
+      hops
+    end
+
+    def links
+    `ip -o link ls`.split("\n").map! { |link| parse_link link }.to_h
+    end
+
+    def foreach_link_change
+      File.popen('ip -o monitor link') do |io|
+        while l = io.readline
+          if l =~ /^Deleted /
+            yield :delete, parse_link(l.gsub!(/^Deleted /, ''))
+          else
+            yield :change, parse_link(l)
+          end
+        end
+      end
+    end
+
+    protected
+    def nexthop(gw, iface, wgt)
+    "nexthop via #{gw} dev #{iface} weight #{wgt}"
+    end
+
+    def run(cmd)
+      puts "#{cmd}: " << `#{cmd}`
+    end
+
+    def parse_link(link)
+      link.scan(/(^\d+): (\w+):/).map! {|id, iface| [iface, id.to_i]}.flatten
     end
   end
-  module_function :add_nexthop
-
-  def del_nexthop(gw, iface)
-    hops = nexthops(iface)
-    run "ip -4 route change default #{hops.join ' '}"
-  end
-  module_function :del_nexthop
-
-  def nexthops(skip = nil)
-    hops = if default_route.include? "\n"
-             default_route.split("\n").reject! { |x| x.strip! == 'default' }
-           else
-             [default_route.sub(/^default/, 'nexthop')]
-           end
-    hops.reject! { |x| x =~ /#{skip}/ } if skip
-    hops
-  end
-  module_function :nexthops
-
-  protected
-  def nexthop(gw, iface, wgt)
-    "nexthop via #{gw} dev #{iface} weight #{wgt}"
-  end
-  module_function :nexthop
-
-  def run(cmd)
-    puts cmd
-#    `#{cmd}`
-  end
-  module_function :run
 end
