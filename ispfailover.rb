06bce7c48613d2f0502f7af1f4d017c52141fb64
Marcello Barnaba <vjt@openssl.it>
2011-04-06 22:03:55 +0200

    whitespace

--- ispfailover.rb
+++ ispfailover.rb
@@ -106,11 +106,11 @@ module ISPFailOver
       if conf[:status] == :alive
         IPRoute.add_nexthop conf[:gateway], conf[:interface], conf[:provider], conf[:weight]
         IPRoute.add_src_route conf[:network], conf[:gateway], conf[:interface], conf[:address], conf[:provider]
-        IPRoute.add_rule    conf[:network], conf[:interface], conf[:provider]
+        IPRoute.add_rule conf[:network], conf[:interface], conf[:provider]
       else
         IPRoute.del_nexthop conf[:gateway], conf[:interface], conf[:provider]
         IPRoute.del_route conf[:network], conf[:gateway], conf[:interface], conf[:provider]
-        IPRoute.del_rule    conf[:network], conf[:interface], conf[:provider]
+        IPRoute.del_rule conf[:network], conf[:interface], conf[:provider]
       end
     end
   end
